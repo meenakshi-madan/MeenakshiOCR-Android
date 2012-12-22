5307196e82c014d56ea586a01631e52f682a0987
Meenakshi Madan <profondeur@rocketmail.com>
2012-12-22 23:11:24 +0530

    Created folders on sdcard successfully and final cropped pic is stored in sdcard as filesocr.jpg Tree structure is /sdcard/Android/data/meenakshi.project.meenakshiocr/ :- files filestessdata filesocr.jpg

--- MeenakshiOCR/src/meenakshi/project/meenakshiocr/MainActivity.java
+++ MeenakshiOCR/src/meenakshi/project/meenakshiocr/MainActivity.java
@@ -2,6 +2,10 @@ package meenakshi.project.meenakshiocr;
 
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +18,11 @@ import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.content.res.AssetManager;
 
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -38,11 +44,23 @@ public class MainActivity extends Activity {
 	private static final int CROP_FROM_CAMERA = 2;
 	private static final int PICK_FROM_FILE = 3;
 	
+	//Fields Specific to OCR
+	//public static final String DATA_PATH = Environment
+			//.getExternalStorageDirectory().getAbsolutePath() + "/MeenakshiOCR/";
+	public String DATA_PATH;
+	public static final String lang = "eng";
+	protected String _path;
+	private static final String TAG = "MainActivity.java";
+	//END OF Fields Specific to OCR
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
+        DATA_PATH = getExternalFilesDir(null).getAbsolutePath();
+        
+        _path = DATA_PATH + "ocr.jpg";
         
         final String [] items			= new String [] {"Take from camera", "Select from gallery"};				
 		ArrayAdapter<String> adapter	= new ArrayAdapter<String> (this, android.R.layout.select_dialog_item,items);
@@ -54,8 +72,11 @@ public class MainActivity extends Activity {
 				if (item == 0) {
 					Intent intent 	 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 					
-					mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
-									   "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+					//mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+					//				   "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+					
+					File file = new File(_path);
+					Uri mImageCaptureUri = Uri.fromFile(file);
 
 					intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
 
@@ -88,6 +109,56 @@ public class MainActivity extends Activity {
 				dialog.show();
 			}
 		});
+        
+        
+        //SPECIFIC TO OCR
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+					Toast.makeText(getApplicationContext(), "Couldn't create required directories. OCR will not work.", Toast.LENGTH_LONG).show();
+				} else {
+					Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+
+		}
+		Log.v(TAG, "End of creating directories for loop");
+		
+		// lang.traineddata file with the app (in assets folder)
+		// This area needs work and optimization
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+			try {
+
+				AssetManager assetManager = getAssets();
+				InputStream in = assetManager.open("tessdata/eng.traineddata");
+				//GZIPInputStream gin = new GZIPInputStream(in);
+				OutputStream out = new FileOutputStream(DATA_PATH
+						+ "tessdata/eng.traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				//while ((lenf = gin.read(buff)) > 0) {
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				//gin.close();
+				out.close();
+				
+				Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+			}
+		}
+		
+		Log.v(TAG, "End of copying traineddata if block");
+		//END OF SPECIFIC TO OCR
+     
     }
     
     @Override
@@ -113,12 +184,23 @@ public class MainActivity extends Activity {
 		        if (extras != null) {	        	
 		            Bitmap photo = extras.getParcelable("data");
 		            
+		            try {
+		                FileOutputStream out = new FileOutputStream(_path);
+		                photo.compress(Bitmap.CompressFormat.JPEG, 90, out);
+		         } catch (Exception e) {
+		                e.printStackTrace();
+		                Log.v(TAG, "Oh noes, couldn't save cropped file to _path");
+		         }
+		            
 		            mImageView.setImageBitmap(photo);
 		        }
 	
-		        File f = new File(mImageCaptureUri.getPath());            
+		        /*COMMENTED CUZ I DON'T WANT TO DELETE THIS FILE, IT'LL BE USED FOR OCR
+		         File f = new File(mImageCaptureUri.getPath());            
+		         
 		        
 		        if (f.exists()) f.delete();
+		        */
 	
 		        break;
 
@@ -148,6 +230,7 @@ public class MainActivity extends Activity {
             //intent.putExtra("aspectY", 1);
             intent.putExtra("scale", true);
             intent.putExtra("return-data", true);
+            //intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
             
         	if (size == 1) {
         		Intent i 		= new Intent(intent);
