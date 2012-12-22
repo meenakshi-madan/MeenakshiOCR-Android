2500ef62c005cd8ba56f8b5a22f95121124918a5
Meenakshi Madan <profondeur@rocketmail.com>
2012-12-22 17:20:12 +0530

    Basic selection from gallery/click from camera and crop. Works nicely.

---
+++ MeenakshiOCR/src/meenakshi/project/meenakshiocr/CropOption.java
@@ -0,0 +1,10 @@
+package meenakshi.project.meenakshiocr;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+public class CropOption {
+	public CharSequence title;
+	public Drawable icon;
+	public Intent appIntent;
+}
