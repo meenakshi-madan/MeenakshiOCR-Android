b21ea0a400bf722b996fe7824d010d8f787aacca
Meenakshi Madan <profondeur@rocketmail.com>
2012-12-23 00:53:33 +0530

    gives very in-accurate ocr but gives some text nonetheless

--- MeenakshiOCR/src/meenakshi/project/meenakshiocr/MainActivity.java
+++ MeenakshiOCR/src/meenakshi/project/meenakshiocr/MainActivity.java
@@ -10,6 +10,9 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.googlecode.tesseract.android.TessBaseAPI;
+import meenakshi.project.meenakshiocr.OCRImageProcessing;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 
@@ -21,11 +24,15 @@ import android.content.pm.ResolveInfo;
 import android.content.res.AssetManager;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
+import android.media.ExifInterface;
 import android.net.Uri;
 
 import android.os.Bundle;
@@ -33,6 +40,7 @@ import android.os.Environment;
 
 import android.widget.Button;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.ImageView;
 
@@ -51,6 +59,7 @@ public class MainActivity extends Activity {
 	public static final String lang = "eng";
 	protected String _path;
 	private static final String TAG = "MainActivity.java";
+	protected TextView _field;
 	//END OF Fields Specific to OCR
 	
     @Override
@@ -58,9 +67,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
-        DATA_PATH = getExternalFilesDir(null).getAbsolutePath();
+        DATA_PATH = getExternalFilesDir(null).getAbsolutePath() + "/";
+        Log.v("Meenakshi", DATA_PATH);
         
-        _path = DATA_PATH + "ocr.jpg";
+        _path = DATA_PATH + "/ocr.jpg";
+		_field = (TextView)findViewById(R.id.textview);
         
         final String [] items			= new String [] {"Take from camera", "Select from gallery"};				
 		ArrayAdapter<String> adapter	= new ArrayAdapter<String> (this, android.R.layout.select_dialog_item,items);
@@ -76,7 +87,7 @@ public class MainActivity extends Activity {
 					//				   "tmp_avatar_" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
 					
 					File file = new File(_path);
-					Uri mImageCaptureUri = Uri.fromFile(file);
+					mImageCaptureUri = Uri.fromFile(file);
 
 					intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
 
@@ -186,13 +197,18 @@ public class MainActivity extends Activity {
 		            
 		            try {
 		                FileOutputStream out = new FileOutputStream(_path);
-		                photo.compress(Bitmap.CompressFormat.JPEG, 90, out);
+		                photo.compress(Bitmap.CompressFormat.JPEG, 100, out);
+		                
+		                File file = new File(_path);
+						mImageCaptureUri = Uri.fromFile(file);
 		         } catch (Exception e) {
 		                e.printStackTrace();
 		                Log.v(TAG, "Oh noes, couldn't save cropped file to _path");
 		         }
 		            
 		            mImageView.setImageBitmap(photo);
+		            //onPhotoTaken();
+		            performOCR();
 		        }
 	
 		        /*COMMENTED CUZ I DON'T WANT TO DELETE THIS FILE, IT'LL BE USED FOR OCR
@@ -284,6 +300,97 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	protected void performOCR()
+	{
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 4;
+
+		Bitmap bitmap = BitmapFactory.decodeFile(_path, options);
+		
+		//bitmap = OCRImageProcessing.makeGreyScale(bitmap);
+		bitmap = OCRImageProcessing.createContrastBW(bitmap, 80);
+
+		try {
+			ExifInterface exif = new ExifInterface(_path);
+			int exifOrientation = exif.getAttributeInt(
+					ExifInterface.TAG_ORIENTATION,
+					ExifInterface.ORIENTATION_NORMAL);
+
+			Log.v(TAG, "Orient: " + exifOrientation);
+
+			int rotate = 0;
+
+			switch (exifOrientation) {
+			case ExifInterface.ORIENTATION_ROTATE_90:
+				rotate = 90;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_180:
+				rotate = 180;
+				break;
+			case ExifInterface.ORIENTATION_ROTATE_270:
+				rotate = 270;
+				break;
+			}
+
+			Log.v(TAG, "Rotation: " + rotate);
+
+			if (rotate != 0) {
+
+				// Getting width & height of the given image.
+				int w = bitmap.getWidth();
+				int h = bitmap.getHeight();
+
+				// Setting pre rotate
+				Matrix mtx = new Matrix();
+				mtx.preRotate(rotate);
+
+				// Rotating Bitmap
+				bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+			}
+
+			// Convert to ARGB_8888, required by tess
+			bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+		} catch (IOException e) {
+			Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+		}
+
+		// _image.setImageBitmap( bitmap );
+		
+		//mImageView.setImageBitmap(bitmap);
+		
+		Log.v(TAG, "Before baseApi");
+
+		TessBaseAPI baseApi = new TessBaseAPI();
+		baseApi.setDebug(true);
+		baseApi.init(DATA_PATH, lang);
+		baseApi.setImage(bitmap);
+		
+		String recognizedText = baseApi.getUTF8Text();
+		
+		baseApi.end();
+
+		// You now have the text in recognizedText var, you can do anything with it.
+		// We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+		// so that garbage doesn't make it to the display.
+
+		Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+		if ( lang.equalsIgnoreCase("eng") ) {
+			recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+		}
+		
+		recognizedText = recognizedText.trim();
+
+		if ( recognizedText.length() != 0 ) {
+			_field.setText(recognizedText);
+			//_field.setText(_field.getText().toString().length() == 0 ? recognizedText : _field.getText() + " " + recognizedText);
+			//_field.setSelection(_field.getText().toString().length());
+		}
+		
+		// Cycle done.
 	}
 
 }
